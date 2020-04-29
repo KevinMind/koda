33d328b249c0eae9916e0bc7fd3075b155084770
kevinmeinhardt <kevin.meinhardt@smava.de>
2020-04-29 11:47:48 +0200

    feat(profile): add local todos UI  with local state
    will replace with api state after

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Auth Starter AWS Amplify',
+    title: 'Gatsby Complete Amplify Starter (Auth, Api)',
   },
   plugins: [
     {
@@ -22,22 +22,6 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-145157132-1',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'https://authenticaysh.netlify.com/',
-      },
-    },
+    'gatsby-plugin-typescript',
   ],
 };
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Button, Container, Grid, Typography, TextField,
+  List, ListItem, ListItemText, ListItemSecondaryAction, IconButton,
+  Paper, Divider,
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { Link } from 'gatsby';
 import withPrivateRoute from '../components/Routes/PrivateRoute';
 import { getCurrentUser } from '../components/Auth/AppUser';
@@ -7,6 +12,23 @@ import { AppContent } from '../components/Layout';
 
 const Profile = () => {
   const user = getCurrentUser();
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = func => ({ target: { value }}) => func(value);
+  const handleSubmit = () => {
+    let list = [...todos, { name, description }];
+    setTodos(list);
+    setName('');
+    setDescription('');
+  };
+  const handleRemove = (idx) => {
+    let list = [ ...todos ];
+    console.log({ idx, list });
+    list.splice(idx, 1);
+    setTodos(list);
+  };
   return (
     <Container>
       <AppContent>
@@ -20,6 +42,72 @@ const Profile = () => {
           {user.phone_number}
         </Typography>
         <Link to="/home">Home</Link>
+        <Grid
+          item
+          container
+          spacing={3}
+          direction="column"
+          justify="space-around"
+          component="form"
+        >
+          <Grid item>
+            <Typography>Add a Todo:</Typography>
+            <Typography>
+              name: {name}
+            </Typography>
+            <Typography>
+              description: {description}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              variant="outlined"
+              name="name"
+              label="name"
+              value={name}
+              fullWidth
+              onChange={handleChange(setName)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              variant="outlined"
+              name="description"
+              label="description"
+              value={description}
+              fullWidth
+              onChange={handleChange(setDescription)}
+            />
+          </Grid>
+          <Grid item>
+            <Button onClick={handleSubmit} variant="contained" color="primary" fullWidth>
+              {'Add Todo +'}
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          spacing={3}
+          direction="column"
+          justify="space-around"
+          component={List}
+        >
+          {todos.map((td, idx) => [
+            <Grid item component={ListItem}>
+              <ListItemText
+                primary={`Name: ${td.name}`}
+                secondary={`Desc: ${td.description}`}
+              />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="delete" onClick={() => handleRemove(idx)}>
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </Grid>,
+            <Divider />
+          ])}
+        </Grid>
       </AppContent>
     </Container>
   );
