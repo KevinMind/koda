43e708558f8b13e5ce09ca80404a55b69b84ea5c
kevinmeinhardt <kevin.meinhardt@smava.de>
2020-05-08 13:34:49 +0200

    feat: add logo fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
+const path = require('path');
+
 const appendScript = require.resolve('./src/custom-sw-code');
+const icon = path.resolve(__dirname, 'src', 'assets', 'images', 'gatsby-icon.png');
+console.log({ icon });
 
 module.exports = {
   siteMetadata: {
@@ -28,25 +32,13 @@ module.exports = {
     'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-plugin-manifest`,
-      icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      icons: [
-        {
-          src: `/favicons/android-chrome-192x192.png`,
-          sizes: `192x192`,
-          type: `image/png`,
-        },
-        {
-          src: `/favicons/android-chrome-512x512.png`,
-          sizes: `512x512`,
-          type: `image/png`,
-        },
-      ],
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        icon: `src/assets/images/gatsby-icon.png`,
+        name: `HappyPuppy`,
+        short_name: `HapPup`,
         start_url: `/`,
         background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
+        theme_color: `#2196f3`,
         display: `fullscreen`,
       },
     },
