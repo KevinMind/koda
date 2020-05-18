a78b251a82a954230a008cf00555d54d8aad2023
kevinmeinhardt <kevin.meinhardt@smava.de>
2020-05-18 23:23:36 +0200

    feat: add date filtering and sorting by date in list view

--- src/components/Pickers/DatePicker.js
+++ src/components/Pickers/DatePicker.js
@@ -3,6 +3,20 @@ import React from 'react';
 import { format as formatDate, set } from 'date-fns';
 import { TextField, Grid } from '@material-ui/core';
 
+export const DatePicker = ({ onChange, label, value, format }) => {
+  return (
+    <TextField
+      size="small"
+      label={label}
+      variant="outlined"
+      fullWidth
+      type="date"
+      onChange={({ target: { value }}) => onChange(new Date(value))}
+      value={formatDate(new Date(value), 'yyyy-MM-dd')}
+    />
+  );
+};
+
 export const DateTimePicker = ({ onChange, label, value, format }) => {
   return (
     <Grid container direction="column" style={{ width: '100%', height: '100%', minHeight: 130 }}>
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { ListItem, ListItemText, ListItemAvatar, List } from '@material-ui/core';
+import { ListItem, ListItemText, ListItemAvatar, List,
+  AppBar, Toolbar, IconButton, Button,
+} from '@material-ui/core';
+import { format as formatDate, isSameDay } from 'date-fns';
 import { Skeleton } from '@material-ui/lab';
 import {navigate} from '@reach/router';
-import { MoreVert, Add, List as ListIcon, CalendarToday } from '@material-ui/icons';
+import { MoreVert, Add, List as ListIcon, CalendarToday, Edit, Close, Delete } from '@material-ui/icons';
 
 import withPrivateRoute from '../../components/Routes/PrivateRoute';
 import { listLogs } from '../../services/log';
@@ -13,6 +16,59 @@ import Layout from '../../components/Layout';
 import { UserNav } from '../../components/Nav';
 import { EventList, EventListItem } from '../../components/Events/EventList';
 import { Categories } from '../../components/Events/data';
+import Dialog from '../../components/Dialog';
+import { DatePicker } from '../../components/Pickers/DatePicker';
+
+const Header = ({ onDone, value }) => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => setOpen(!open);
+  const handleDone = ()=> {
+    setOpen(false);
+    onDone(date);
+  };
+
+  return (
+    <React.Fragment>
+      <Dialog animation="fade" open={open}>
+        <Dialog.Title>
+          <IconButton onClick={toggle}>
+            <Close />
+          </IconButton>
+        </Dialog.Title>
+        <Dialog.Content style={{ padding: 30 }}>
+          <DatePicker value={date} onChange={setDate} />
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button
+            fullWidth
+            onClick={handleDone}
+            variant="contained"
+            color="primary"
+          >
+            Done
+          </Button>
+        </Dialog.Actions>
+      </Dialog>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton onClick={toggle}>
+            <Edit />
+          </IconButton>
+          {value && [
+            <div>
+              Date: {formatDate(new Date(value), 'dd/MM/yyyy')}
+            </div>,
+            <IconButton onClick={() => onDone()}>
+              <Delete />
+            </IconButton>
+          ]}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
 
 const LoadingItem = () => (
   <ListItem style={{ width: '100%' }}>
@@ -46,9 +102,10 @@ const processList = (events) => events.map(({ start, end, ...rest }) => ({
   start: new Date(start),
   end: new Date(end),
   ...rest,
-}));
+})).sort((a, b) => b.start - a.start);
 
 const Events = () => {
+  const [date, setDate] = useState();
   const [loaded, setLoaded] = useState(false);
   const [list, setList] = useState([]);
   const [curr, setCurr] = useState();
@@ -135,6 +192,12 @@ const Events = () => {
         </ConfirmEvent>
       )}
       <Layout
+        Header={
+          <Header
+            onDone={setDate}
+            value={date}
+          />
+        }
         Footer={
           <UserNav>
             <UserNav.Item
@@ -166,7 +229,13 @@ const Events = () => {
                 onSwipeLeft={handleSwipeLeft}
                 onSwipeRight={handleSwipeRight}
               >
-                {list.map((item) => {
+                {list
+                  .filter(({ start }) => {
+                    console.log({ date, start });
+                    return date ? isSameDay(start, date) : true;
+
+                  })
+                  .map((item) => {
                   const { color } = Categories.find(cat => cat.label === item.category);
                   return (
                     <EventListItem
